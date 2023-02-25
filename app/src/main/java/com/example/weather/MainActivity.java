f31149ac94a9a794c0b08816d73c91f1b9b6c9e4
biswa_rx <biswaranjanbeheraqazmlp@gmail.com>
2023-02-25 21:43:22 +0530

    Request Location permission on main activity...working
    Error found in
    ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED)
    
    it become not equal

--- app/src/main/java/com/example/weather/MainActivity.java
+++ app/src/main/java/com/example/weather/MainActivity.java
@@ -1,17 +1,34 @@
 package com.example.weather;
 
 import androidx.annotation.DrawableRes;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.recyclerview.widget.LinearLayoutManager;
 
+import android.Manifest;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.drawable.Drawable;
+import android.location.Location;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
 import com.example.weather.Adapters.RvSpreedAdapter;
 import com.example.weather.Models.RvSpreedModel;
 import com.example.weather.databinding.ActivityMainBinding;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 
 import java.util.ArrayList;
 
@@ -26,8 +43,12 @@ public class MainActivity extends AppCompatActivity implements RvSpreedAdapter.O
     ActivityMainBinding mainBinding;
     ArrayList<RvSpreedModel> rvList;
     private JsonPlaceHolderApi jsonPlaceHolderApi;
-
     private WeatherInfo weatherInfo;
+
+    int REQUEST_CODE_FINE_LOCATION = 10001;
+    private static final String TAG = "MainActivity";
+    FusedLocationProviderClient fusedLocationProviderClient;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -42,12 +63,16 @@ public class MainActivity extends AppCompatActivity implements RvSpreedAdapter.O
 
         jsonPlaceHolderApi = retrofit.create(JsonPlaceHolderApi.class);
 
-        getForcast();
-
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            getLastLocation();
+        } else {
+            askLocationPermission();
+        }
     }
 
-    private void getForcast() {
-        Call<WeatherInfo> weatherInfoCall = jsonPlaceHolderApi.getWeatherInfo(-3.255703,-62.084950);
+    private void getForcast(double latitude,double longitude) {
+        Call<WeatherInfo> weatherInfoCall = jsonPlaceHolderApi.getWeatherInfo(latitude,longitude);
 //Enqueue method for not in ui thread it uses separate thread...
         weatherInfoCall.enqueue(new Callback<WeatherInfo>() {
             @Override
@@ -121,6 +146,91 @@ public class MainActivity extends AppCompatActivity implements RvSpreedAdapter.O
 //            mainBinding.ivWeatherDesc.setImageDrawable(drawable);
             mainBinding.ivWeatherDesc.setImageResource(new WeatherType().getWeatherType(wtCode).getWeatherIcon());
             mainBinding.cardView.setVisibility(View.VISIBLE);
+        }
+    }
+
+
+    //////////////////Copy from splash Activity
+
+    private void askLocationPermission() {
+        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, android.Manifest.permission.ACCESS_FINE_LOCATION)) {
+                new AlertDialog.Builder(this)
+                        .setMessage("We need permission for fine location")
+                        .setCancelable(false)
+                        .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                ActivityCompat.requestPermissions(MainActivity.this, new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_CODE_FINE_LOCATION);
+                            }//Second Time
+                        })
+                        .show();
+
+            } else {
+
+                ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_CODE_FINE_LOCATION);
+            }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+
+        if (requestCode == REQUEST_CODE_FINE_LOCATION) {
+
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                //Permission Granted
+                getLastLocation();
+            } else {
+                //Permission NOT granted
+                if (!ActivityCompat.shouldShowRequestPermissionRationale(this, android.Manifest.permission.ACCESS_FINE_LOCATION)) {
+                    //This block here means PERMANENTLY DENIED PERMISSION
+                    new AlertDialog.Builder(this)
+                            .setMessage("You have permanently denied this permission, go to settings to enable this permission")
+                            .setPositiveButton("Go to settings", new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialogInterface, int i) {
+                                    gotoApplicationSettings();
+                                }
+                            })
+                            .setNegativeButton("Cancel", null)
+                            .setCancelable(false)
+                            .show();
+                }
+            }
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+
+    private void gotoApplicationSettings() {
+
+        Intent intent = new Intent();
+        intent.setAction(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+        Uri uri = Uri.fromParts("package", this.getPackageName(), null);
+        intent.setData(uri);
+        startActivity(intent);
+    }
+
+    private void getLastLocation() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            Task<Location> locationTask = fusedLocationProviderClient.getLastLocation();
+            locationTask.addOnSuccessListener(new OnSuccessListener<Location>() {
+                @Override
+                public void onSuccess(Location location) {
+                    if(location != null){
+                        Log.e(TAG,"Latitude: "+location.getLatitude());
+                        Log.e(TAG,"Longitude: "+location.getLongitude());
+                        getForcast(location.getLatitude(),location.getLongitude());
+                    }
+                }
+            });
+            locationTask.addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    Log.e(TAG,"OnFailure: "+e.getLocalizedMessage());
+                }
+            });
         }
     }
 }
--- app/src/main/java/com/example/weather/SplashActivity.java
+++ app/src/main/java/com/example/weather/SplashActivity.java
@@ -23,99 +23,10 @@ import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 
 public class SplashActivity extends AppCompatActivity {
-    int REQUEST_CODE_FINE_LOCATION = 10001;
-    private static final String TAG = "SplashActivity";
-    FusedLocationProviderClient fusedLocationProviderClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
-        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
-            getLastLocation();
-        } else {
-            askLocationPermission();
-        }
-    }
-
-    private void askLocationPermission() {
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
-            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_FINE_LOCATION)) {
-                new AlertDialog.Builder(SplashActivity.this)
-                        .setMessage("We need permission for fine location")
-                        .setCancelable(false)
-                        .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialogInterface, int i) {
-                                ActivityCompat.requestPermissions(SplashActivity.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_CODE_FINE_LOCATION);
-                            }//Second Time
-                        })
-                        .show();
-
-            } else {
-                ActivityCompat.requestPermissions(SplashActivity.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_CODE_FINE_LOCATION);
-            }
-        }
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-
-        if (requestCode == REQUEST_CODE_FINE_LOCATION) {
-
-            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                //Permission Granted
-                getLastLocation();
-            } else {
-                //Permission NOT granted
-                if (!ActivityCompat.shouldShowRequestPermissionRationale(SplashActivity.this, Manifest.permission.ACCESS_FINE_LOCATION)) {
-                    //This block here means PERMANENTLY DENIED PERMISSION
-                    new AlertDialog.Builder(SplashActivity.this)
-                            .setMessage("You have permanently denied this permission, go to settings to enable this permission")
-                            .setPositiveButton("Go to settings", new DialogInterface.OnClickListener() {
-                                @Override
-                                public void onClick(DialogInterface dialogInterface, int i) {
-                                    gotoApplicationSettings();
-                                }
-                            })
-                            .setNegativeButton("Cancel", null)
-                            .setCancelable(false)
-                            .show();
-                }
-            }
-        }
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-    }
-
-
-    private void gotoApplicationSettings() {
-
-        Intent intent = new Intent();
-        intent.setAction(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
-        Uri uri = Uri.fromParts("package", this.getPackageName(), null);
-        intent.setData(uri);
-        startActivity(intent);
-    }
-
-    private void getLastLocation() {
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
-            Task<Location> locationTask = fusedLocationProviderClient.getLastLocation();
-            locationTask.addOnSuccessListener(new OnSuccessListener<Location>() {
-                @Override
-                public void onSuccess(Location location) {
-                    if(location != null){
-                        Log.e(TAG,"Latitude: "+location.getLatitude());
-                        Log.e(TAG,"Longitude: "+location.getLongitude());
-                    }
-                }
-            });
-            locationTask.addOnFailureListener(new OnFailureListener() {
-                @Override
-                public void onFailure(@NonNull Exception e) {
-                    Log.e(TAG,"OnFailure: "+e.getLocalizedMessage());
-                }
-            });
-        }
+        startActivity(new Intent(this,MainActivity.class));
     }
 }
